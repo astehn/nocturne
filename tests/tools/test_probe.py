ff82b3d103e902694c36da9bb84c3cd1fe2ddb9c
Andreas Stehn <andreas@stehn.com>
2026-06-30 13:42:32 +0200

    feat: add binary probe helper

---
+++ tests/tools/test_probe.py
@@ -0,0 +1,21 @@
+from seestar_processor.tools.probe import probe_binary
+
+
+def test_probe_success():
+    ok, msg = probe_binary("/x", ["-v"], runner=lambda a: (0, "GraXpert 3.1.0\n", ""))
+    assert ok and "GraXpert" in msg
+
+
+def test_probe_uses_stderr_when_stdout_empty():
+    ok, msg = probe_binary("/x", ["-v"], runner=lambda a: (0, "", "RC-Astro 0.9.3"))
+    assert ok and "RC-Astro" in msg
+
+
+def test_probe_failure():
+    ok, msg = probe_binary("/x", ["-v"], runner=lambda a: (1, "", "boom"))
+    assert not ok and "boom" in msg
+
+
+def test_probe_missing_binary():
+    ok, msg = probe_binary("/nope/definitely-not-here", ["-v"])
+    assert ok is False and msg
